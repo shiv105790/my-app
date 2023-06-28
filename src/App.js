5088047e394f0410479bcc4875afd65603e1df02
shiv105790 <designer.eww@gmail.com>
2023-06-28 11:16:41 +0530

    some changes"

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from "react"
+import React from "react";
 import My_app from "./My_app";
 import Tiktok from "./Tik Tok/Tiktok";
 
 function App() {
-    return (
-        <div>
-            <My_app />
-            {/* <Tiktok /> */}
-        </div>
-    )
+  return (
+    <div>
+      {/* <My_app /> */}
+      {/* <Tiktok /> */}
+    </div>
+  );
 }
 
 export default App;
